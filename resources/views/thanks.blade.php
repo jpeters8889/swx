040f404d764294989854b6a652085999f975a442
Jamie Peters <jamie@jamie-peters.co.uk>
2020-08-28 19:30:18 +0100

    update_back_to_group_link

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -5,12 +5,16 @@
         Thank you, your booking was successful, we have sent you an email confirmation with
         the date and time of your group!
     </p>
-    <h2 class="text-lg my-2">Booking Details</h2>
+    <h2 class="text-lg my-2 font-semibold text-sw-red">Booking Details</h2>
     <p>
         {{ $booking->groupSession->group->name }} with {{ $booking->groupSession->group->user->first_name }},
-        on {{ $booking->groupSession->date->format('l jS F Y') }} at {{ $booking->groupSession->session->human_start_time }}.
+        on {{ $booking->groupSession->date->format('l jS F Y') }}
+        at {{ $booking->groupSession->session->human_start_time }}.
     </p>
-    <p class="mt-2">
-        <a class="font-semibold hover:underline" href="/{{ $booking->groupSession->group->slug }}">Back to group...</a>
-    </p>
+    <div class="mt-4 flex justify-center">
+        <a class="bg-sw-red text-white leading-none py-4 px-6 rounded-lg text-lg font-semibold hover:bg-sw-red-80 transition-bg"
+           href="/{{ $booking->groupSession->group->slug }}">
+            Back to {{ $booking->groupSession->group->name }}
+        </a>
+    </div>
 @endsection
