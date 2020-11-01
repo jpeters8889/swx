111626b33badd180b453a436d233635fe57bb95e
Jamie Peters <jamie@jamie-peters.co.uk>
2020-11-01 09:22:15 +0000

    hide groups with no live sessions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Group;
+use App\Models\GroupSession;
+use App\Models\Session;
 use JPeters\PageViewBuilder\Page;
 
 class HomeController
@@ -10,6 +12,7 @@ class HomeController
     public function get(Page $page)
     {
         $groups = Group::query()
+            ->whereHas('sessions', fn(Session $session) => $session->live === 1)
             ->with(['sessions', 'sessions.day', 'user'])
             ->orderBy('order')
             ->orderBy('name')
