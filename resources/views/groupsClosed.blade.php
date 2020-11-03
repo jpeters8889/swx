6e87550aec482105b783a58a2a25b84e587fa1fa
Jamie Peters <jamie@jamie-peters.co.uk>
2020-11-03 18:01:32 +0000

    maintenance mode

--- resources/views/groupsClosed.blade.php
+++ resources/views/groupsClosed.blade.php
@@ -3,29 +3,29 @@
 @section('content')
     <h2 class="text-lg my-2 font-semibold text-sw-red">Physical Groups Temporally Closed</h2>
 
-    <p>
+    <p class="mb-2">
         Unfortunately due to the current lockdown in England we've had to pause our physical Slimming World Groups at
         The Brittles, Wistaston.
     </p>
 
-    <p>
+    <p class="mb-2">
         Rest assured though, we're still committed to supporting your weight loss journey which is why we're still
         running our <strong>Virtual Groups!</strong>
     </p>
 
-    <p>
+    <p class="mb-2">
         With our Virtual Groups you just need to weigh at home and send us your weight every week, and for that 'in
         group' experience we'll be running Image Therapy sessions from the comfort of your own home like we did in the
         previous lockdown between March and August.
     </p>
 
-    <p>
+    <p class="mb-2">
         For those of you currently on a countdown, these will not be paused and will remain active, in buying the
         countdown you have committed to either 6 or 12 weeks of your weight loss journey and we will continue to support
         you in that journey. Any weeks not used will unfortunately be lost.
     </p>
 
-    <p>
+    <p class="mb-2">
         For more information please get in touch, and hopefully we'll be back in physical groups in December!
     </p>
 
