18b0f7f21fa42ecefd069a96a564fde74f0ef00e
Jamie Peters <jamie@jamie-peters.co.uk>
2020-12-23 16:25:31 +0000

    update maintenance page

--- resources/views/groupsClosed.blade.php
+++ resources/views/groupsClosed.blade.php
@@ -1,33 +1,42 @@
 @extends('layouts.slimming-world')
 
 @section('content')
-    <h2 class="text-lg my-2 font-semibold text-sw-red">Physical Groups Temporally Closed</h2>
+{{--    <h2 class="text-lg my-2 font-semibold text-sw-red">Physical Groups Temporally Closed</h2>--}}
+    <h2 class="text-lg my-2 font-semibold text-sw-red">Cheshire Tier 3 Restrictions</h2>
 
     <p class="mb-2">
-        Unfortunately due to the current lockdown in England we've had to pause our physical Slimming World Groups at
-        The Brittles, Wistaston.
+        Due to Cheshire entering Tier 3 Restrictions from boxing day we're currently makings plan on how to operate groups as weight and go sessions.
     </p>
 
     <p class="mb-2">
-        Rest assured though, we're still committed to supporting your weight loss journey which is why we're still
-        running our <strong>Virtual Groups!</strong>
+        Please come back soon for updates, and I'll be in touch with more information soon!
     </p>
 
-    <p class="mb-2">
-        With our Virtual Groups you just need to weigh at home and send us your weight every week, and for that 'in
-        group' experience we'll be running Image Therapy sessions from the comfort of your own home like we did in the
-        previous lockdown between March and August.
-    </p>
+{{--    <p class="mb-2">--}}
+{{--        Unfortunately due to the current lockdown in England we've had to pause our physical Slimming World Groups at--}}
+{{--        The Brittles, Wistaston.--}}
+{{--    </p>--}}
 
-    <p class="mb-2">
-        For those of you currently on a countdown, these will not be paused and will remain active, in buying the
-        countdown you have committed to either 6 or 12 weeks of your weight loss journey and we will continue to support
-        you in that journey. Any weeks not used will unfortunately be lost.
-    </p>
+{{--    <p class="mb-2">--}}
+{{--        Rest assured though, we're still committed to supporting your weight loss journey which is why we're still--}}
+{{--        running our <strong>Virtual Groups!</strong>--}}
+{{--    </p>--}}
 
-    <p class="mb-2">
-        For more information please get in touch, and hopefully we'll be back in physical groups in December!
-    </p>
+{{--    <p class="mb-2">--}}
+{{--        With our Virtual Groups you just need to weigh at home and send us your weight every week, and for that 'in--}}
+{{--        group' experience we'll be running Image Therapy sessions from the comfort of your own home like we did in the--}}
+{{--        previous lockdown between March and August.--}}
+{{--    </p>--}}
+
+{{--    <p class="mb-2">--}}
+{{--        For those of you currently on a countdown, these will not be paused and will remain active, in buying the--}}
+{{--        countdown you have committed to either 6 or 12 weeks of your weight loss journey and we will continue to support--}}
+{{--        you in that journey. Any weeks not used will unfortunately be lost.--}}
+{{--    </p>--}}
+
+{{--    <p class="mb-2">--}}
+{{--        For more information please get in touch, and hopefully we'll be back in physical groups in December!--}}
+{{--    </p>--}}
 
     <p>
         Alison x
