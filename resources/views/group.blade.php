19b2fc01e3207d6b64e4cbf62aeadb985f70401f
Jamie Peters <jamie@jamie-peters.co.uk>
2021-07-14 18:50:11 +0100

    update group information

--- resources/views/group.blade.php
+++ resources/views/group.blade.php
@@ -16,68 +16,75 @@
 
             <accordion>
                 <template v-slot:title>
-                    Click here to read the safety measures we have in place at our groups.
+                    Click here to read an important update for groups from the 19th July.
                 </template>
 
                 <p class="mb-2">
-                    We've got lots of measures in place to keep you safe at group, including compulsory booking for all
-                    attendees, other safety measures include:
+                    Our groups are starting to slowly return to 'normal', but we still have lots of measures in place to
+                    keep you safe at group.
                 </p>
 
                 <ul class="list-disc ml-4">
                     <li>
-                        Hand Sanitizer is available at the pay station, weigh station and the shop, but feel free to
+                        Masks aren't compulsory, but unless you are exempt we would ask that you wear a mask while
+                        walking round the venue and at pay and weigh, but you can take it off when you're sat in your
+                        seat for group.
+                    </li>
+                    <li>
+                        Hand Sanitizer is available is available at several points around the venue, but feel free to
                         bring your own too if you like.
                     </li>
                     <li>
-                        Masks <strong>must</strong> be worn while in group, unless you are exempt under government
-                        guidelines.
+                        We will still be operating a separate entry/exit door, entry will be via the rear door and the
+                        exit via the middle door straight onto the car park.
                     </li>
                     <li>
-                        There will be a one-way system in the venue and queueing outside the seating area.
+                        Shoes are no longer required when you are getting weighed, and we will be able to write in your
+                        books again.
                     </li>
                     <li>
-                        We will be operating a separate entry/exit door, entry will be via the rear door and the exit
-                        via the middle door straight onto the car park.
+                        At the shop, if possible only touch what you are planning on buying.
                     </li>
                     <li>
-                        Sneeze screens will in place around the pay station and weigh station and to protected the
-                        consultant and social team.
+                        All surfaces, and pay and weigh equipment will be cleaned between sessions.
+                    </li>
+                </ul>
+
+                <ul>
+                    <li>
+                        We are removing the sneeze screens, social distance dots and one way system around the pay and
+                        weigh stations.
                     </li>
                     <li>
-                        Please pay contactless where possible. If you need to pay by Chip and Pin we will provide a
-                        single use glove for handling the terminal.
+                        For the time being our Tuesday morning group will be sticking at two shorter sessions.
                     </li>
                     <li>
-                        Cash will be accepted, but please use the correct change where possible.
+                        Our Tuesday evening group will combine into one 90 minute session with 30 minute weigh period
+                        and 60 minute IMAGE therapy group session.
                     </li>
                     <li>
-                        Shoes <strong>MUST</strong> be worn on scales when you are weighing.
+                        Our Thursday evening groups will stay as two separate sessions at the same time, but be extended
+                        into 90 minute sessions with a 30 minute weigh period and 60 minute IMAGE therapy group session.
                     </li>
                     <li>
-                        Please bring your own pen to write your weight in your book.
+                        New Member sessions will be at the same time as weigh at normal groups, rather than a dedicated
+                        session between or after sessions.
                     </li>
                     <li>
-                        When weighed please take the first available seat and please do not move seat after sitting
-                        down.
+                        Refreshments including tea and coffee will be available from the kitchen. Please wipe down after
+                        use!
                     </li>
                     <li>
-                        If you place your belongings on a seat before weighing you must sit in that seat.
-                    <li>
-                        Toilets are strictly emergency only, so please go before attending group.
+                        Toilets will be back open for use during groups.
                     </li>
                     <li>
-                        Any items that you handle from the shop, but do not purchase must be placed in a box for
-                        decontamination.
+                        Over the coming weeks we will slowly reintroduce previous group activities such as the raffle,
+                        slimmer of the week basket, interest table.
                     </li>
                     <li>
-                        There are no refreshments available, so please bring your own drinks if required.
-                    </li>
-                    <li>
-                        There is no raffle or slimmer of the week basket at present.
-                    </li>
-                    <li>
-                        All surfaces, and pay and weight equipment will be cleaned between sessions.
+                        Over the coming weeks we will start rearranging the group layout to how it was pre-covid with
+                        the pay and weigh stations at the opposite end of the building to where they are now, and
+                        introducing more seats and seats closer together.
                     </li>
                 </ul>
             </accordion>
